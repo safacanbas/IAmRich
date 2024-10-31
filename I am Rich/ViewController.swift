d4bdb25ec6754f4c8d20a33d63bfe10c95c5d501
Safa Can BAŞ <bassafacan@gmail.com>
2024-10-31 22:15:59 +0300

    Project Files
    Project files uploaded.

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Safa Can BAŞ on 29.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
